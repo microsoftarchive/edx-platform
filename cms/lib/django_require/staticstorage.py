128e14ff67ca30c302ac92a1582ab63633117eb3
polesye <s2pak.anton@gmail.com>
2014-10-09 14:46:37 +0300

    Add docstring.

--- cms/lib/django_require/staticstorage.py
+++ cms/lib/django_require/staticstorage.py
@@ -3,4 +3,7 @@ from require.storage import OptimizedFilesMixin
 
 
 class OptimizedCachedRequireJSStorage(OptimizedFilesMixin, PipelineStorage):
+    """
+    Custom storage backend that is used by Django-require.
+    """
     pass
