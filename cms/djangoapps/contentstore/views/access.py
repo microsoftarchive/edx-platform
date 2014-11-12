77219f770b2eefc700870479724777bd6438d0a1
Braden MacDonald <braden@opencraft.com>
2014-11-11 19:38:39 -0800

    Update the REST API course_team_handler to support library roles, fix bugs

--- cms/djangoapps/contentstore/views/access.py
+++ cms/djangoapps/contentstore/views/access.py
@@ -8,6 +8,44 @@ from student.roles import (
 from student import auth
 
 
+# Studio permissions:
+EDIT_ROLES = 8
+VIEW_USERS = 4
+EDIT_CONTENT = 2
+VIEW_CONTENT = 1
+# In addition to the above, one is always allowed to "demote" oneself to a lower role within a course, or remove oneself.
+
+
+def get_user_permissions(user, course_key, org=None):
+    """
+    Get the set() of permissions that this user has in the given course context.
+    Can also set course_key=None and pass in an org to get the user's
+    permissions for that organization as a whole.
+
+    These permissions are specific to studio, but the roles that define them are
+    shared with the LMS.
+    """
+    if org is None:
+        org = course_key.org
+        course_key = course_key.for_branch(None)
+    else:
+        assert course_key is None
+    all_perms = {EDIT_ROLES, VIEW_USERS, EDIT_CONTENT, VIEW_CONTENT}
+    # global staff, org instructors, and course instructors have all permissions:
+    if GlobalStaff().has_user(user) or OrgInstructorRole(org=org).has_user(user):
+        return all_perms
+    if course_key and auth.has_access(user, CourseInstructorRole(course_key)):
+        return all_perms
+    # Staff have a all permissions except EDIT_ROLES:
+    if OrgStaffRole(org=org).has_user(user) or (course_key and auth.has_access(user, CourseStaffRole(course_key))):
+        return {VIEW_USERS, EDIT_CONTENT, VIEW_CONTENT}
+    # Otherwise, for libraries, users can view only:
+    if (course_key and isinstance(course_key, LibraryLocator)):
+        if OrgLibraryUserRole(org=org).has_user(user) or auth.has_access(user, LibraryUserRole(course_key)):
+            return {VIEW_USERS, VIEW_CONTENT}
+    return set()
+
+
 def has_course_access(user, course_key, role=CourseStaffRole):
     """
     Return True if user allowed to access this course_id
@@ -33,7 +71,7 @@ def has_write_access(user, course_key):
     Return True iff user is allowed to modify the given course/library.
     Currently equivalent to has_course_access but less amibguously named.
     """
-    return has_course_access(user, course_key)
+    return EDIT_CONTENT in get_user_permissions(user, course_key)
 
 
 def has_read_access(user, course_key):
@@ -43,13 +81,7 @@ def has_read_access(user, course_key):
     There is currently no such thing as read-only course access in studio, but
     there is read-only access to content libraries.
     """
-    if has_course_access(user, course_key):
-        return True  # Global, Org, or Course "Instructors" and "Staff" can read and write
-    if isinstance(course_key, LibraryLocator):
-        if OrgLibraryUserRole(org=course_key.org).has_user(user):
-            return True  # User has read-only access to all libraries in this organization
-        return LibraryUserRole(course_key.for_branch(None)).has_user(user)  # User has read-only access this library
-    return False
+    return VIEW_CONTENT in get_user_permissions(user, course_key)
 
 
 def get_user_role(user, course_id):
