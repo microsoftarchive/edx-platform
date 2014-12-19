8a0d7d0cac70680263c29ee529e91a7896e6f785
asadiqbal08 <aiqbal@edx.org>
2014-12-19 11:50:06 +0500

    Bok-Choy Studio Test PRC

--- common/test/acceptance/pages/studio/settings.py
+++ common/test/acceptance/pages/studio/settings.py
@@ -3,7 +3,11 @@ Course Schedule and Details Settings page.
 """
 
 from .course_page import CoursePage
+from selenium.webdriver.support.ui import Select
+from nose.tools import set_trace
 
+PRE_REQUISITE_COURSE_SELECTOR = "pre-requisite-course"
+SAVE_BUTTON_SELECTOR = ".button.action-primary.action-save"
 
 class SettingsPage(CoursePage):
     """
@@ -14,3 +18,45 @@ class SettingsPage(CoursePage):
 
     def is_browser_on_page(self):
         return self.q(css='body.view-settings').present
+
+    @property
+    def pre_requisite_course(self):
+        """
+        Returns the pre-requisite course drop down field.
+        """
+        return self.q(css='#pre-requisite-course')
+
+    @property
+    def save_changes_button(self):
+        """
+        Returns the pre-requisite course drop down field.
+        """
+        return self.q(css='a.button.action-primary.action-save')
+
+    def trigger_save_changes_button(self):
+        """
+        Trigger the save changes button.
+        """
+        self.q(css=SAVE_BUTTON_SELECTOR).click()
+        self.wait_for_ajax()
+
+    def select_pre_requisite_course_from_drop_down(self, index):
+        """
+        Select the course from drop down at given index.
+        """
+        select = Select(self.browser.find_element_by_id("{selector}".format(selector=PRE_REQUISITE_COURSE_SELECTOR)))
+        select.select_by_index(index)
+
+    def is_pre_requisite_course_selected(self):
+        """
+        Check either a course is selected in pre requisite courses drop down
+        """
+        select = Select(self.browser.find_element_by_id("{selector}".format(selector=PRE_REQUISITE_COURSE_SELECTOR)))
+        return (select.first_selected_option.is_selected() and select.first_selected_option.text != 'None')
+
+    def refresh_page(self):
+        """
+        Reload the page.
+        """
+        self.browser.refresh()
+
--- common/test/acceptance/tests/studio/test_studio_settings_details.py
+++ common/test/acceptance/tests/studio/test_studio_settings_details.py
@@ -2,7 +2,8 @@
 Acceptance tests for Studio's Settings Details pages
 """
 from acceptance.tests.studio.base_studio_test import StudioCourseTest
-
+from nose.tools import set_trace
+from ...fixtures.course import CourseFixture
 from ...pages.studio.settings import SettingsPage
 
 
@@ -11,7 +12,7 @@ class SettingsMilestonesTest(StudioCourseTest):
     Tests for milestones feature in Studio's settings tab
     """
     def setUp(self):
-        super(SettingsMilestonesTest, self).setUp()
+        super(SettingsMilestonesTest, self).setUp(is_staff=True)
         self.settings_detail = SettingsPage(
             self.browser,
             self.course_info['org'],
@@ -23,9 +24,45 @@ class SettingsMilestonesTest(StudioCourseTest):
         self.settings_detail.visit()
         self.assertTrue(self.settings_detail.is_browser_on_page())
 
-    def test_page_has_prerequisite_field(self):
-        """
-        Test to make sure page has pre-requisite course field if milestones app is enabled.
-        """
+    # def test_page_has_prerequisite_field(self):
+    #     """
+    #     Test to make sure page has pre-requisite course field if milestones app is enabled.
+    #     """
+    #
+    #     self.assertTrue(self.settings_detail.pre_requisite_course.present)
 
-        self.assertTrue(self.settings_detail.q(css='#pre-requisite-course').present)
+    def test_prerequisite_course_save_successfully(self):
+        """
+        Test to make sure pre-requisite course field successfully saving the changes.
+        """
+        c_f = CourseFixture(
+            'test_org1',
+            self.course_info['number'],
+            self.course_info['run'],
+            self.course_info['display_name']
+        )
+        c_f.install()
+
+        set_trace()
+        # self.settings_detail.select_pre_requisite_course_from_drop_down(index=1)
+        # self.assertTrue(self.settings_detail.save_changes_button.present)
+        #
+        # # # now trigger the save changes button.
+        # self.settings_detail.trigger_save_changes_button();
+        # self.assertTrue('Your changes have been saved.' in self.settings_detail.browser.page_source)
+        # self.settings_detail.refresh_page();
+        # self.assertTrue(self.settings_detail.is_pre_requisite_course_selected())
+        # set_trace()
+        #
+        #
+        # # now reset/update the pre requisite course
+        # self.settings_detail.select_pre_requisite_course_from_drop_down(index=0)
+        # self.assertTrue(self.settings_detail.save_changes_button.present)
+        #
+        #
+        # # # now trigger the save changes button to update the pre requisite course selection.
+        # self.settings_detail.trigger_save_changes_button();
+        # self.assertTrue('Your changes have been saved.' in self.settings_detail.browser.page_source)
+        # self.assertFalse(self.settings_detail.is_pre_requisite_course_selected())
+
+
