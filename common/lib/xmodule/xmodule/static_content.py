b1c7f5b29c7f8b69b41ba4f60c9f4f0972c316f2
Ben Patterson <bpatterson@edx.org>
2015-02-27 12:13:33 -0500

    Fix paths.

--- common/lib/xmodule/xmodule/static_content.py
+++ common/lib/xmodule/xmodule/static_content.py
@@ -96,9 +96,9 @@ def _write_styles(selector, output_root, classes):
             css_imports[class_].add(fragment_name)
 
     module_styles_lines = []
-    module_styles_lines.append("@import 'vendor/bourbon/bourbon';")
-    module_styles_lines.append("@import 'vendor/bourbon/addons/button';")
-    module_styles_lines.append("@import 'assets/anims';")
+    module_styles_lines.append("@import 'sass/vendor/bourbon/bourbon';")
+    module_styles_lines.append("@import 'sass/vendor/bourbon/addons/button';")
+    module_styles_lines.append("@import 'sass/assets/anims';")
     for class_, fragment_names in css_imports.items():
         module_styles_lines.append("""{selector}.xmodule_{class_} {{""".format(
             class_=class_, selector=selector
