f2b4d4ebcf66a058f82be4caa6a1631108b706a2
Sarina Canelake <sarina@edx.org>
2015-03-23 14:39:13 -0400

    DO NOT MERGE: Enable idde feature

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -208,7 +208,7 @@ FEATURES = {
     'ENABLE_INSTRUCTOR_BACKGROUND_TASKS': True,
 
     # Enable instructor to assign individual due dates
-    'INDIVIDUAL_DUE_DATES': False,
+    'INDIVIDUAL_DUE_DATES': True,
 
     # Enable legacy instructor dashboard
     'ENABLE_INSTRUCTOR_LEGACY_DASHBOARD': True,
