d3bde0340bc50ab35ac5c26330ab0db88531d8ea
Davorin Sego <davorin.sego@gmail.com>
2015-04-07 22:55:02 +0200

    remove console.log

--- lms/static/js/search/base/views/search_results_view.js
+++ lms/static/js/search/base/views/search_results_view.js
@@ -54,7 +54,6 @@ define([
                 this.$el.find('.search-load-next').remove();
             }
             this.$el.find(this.spinner).hide();
-            console.log(this.collection.models)
         },
 
         renderItems: function () {
