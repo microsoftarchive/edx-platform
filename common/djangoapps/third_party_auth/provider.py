b5b16fa3dcf021291cf8160a8694337911e77749
nunpa <nunpa@qrz27.com>
2015-04-21 23:19:18 +0200

    Azure AD support on providers

--- common/djangoapps/third_party_auth/provider.py
+++ common/djangoapps/third_party_auth/provider.py
@@ -4,7 +4,7 @@ Loaded by Django's settings mechanism. Consequently, this module must not
 invoke the Django armature.
 """
 
-from social.backends import google, linkedin, facebook
+from social.backends import google, linkedin, facebook, azuread
 
 _DEFAULT_ICON_CLASS = 'fa-signin'
 
@@ -169,6 +169,25 @@ class FacebookOauth2(BaseProvider):
     def get_name(cls, provider_details):
         return provider_details.get('fullname')
 
+class AzureADOauth2(BaseProvider):
+    """Provider for Azure AD's Oauth2 auth system."""
+
+    BACKEND_CLASS = azuread.AzureADOAuth2
+    ICON_CLASS = 'fa-windows'
+    NAME = 'AzureAD'
+    SETTINGS = {
+        'SOCIAL_AUTH_AZUREAD_OAUTH2_KEY': None,
+        'SOCIAL_AUTH_AZUREAD_OAUTH2_SECRET': None,
+        'SOCIAL_AUTH_AZUREAD_OAUTH2_SHAREPOINT_SITE': None
+    }
+
+    @classmethod
+    def get_email(cls, provider_details):
+        return provider_details.get('email')
+
+    @classmethod
+    def get_name(cls, provider_details):
+        return provider_details.get('fullname')
 
 class Registry(object):
     """Singleton registry of third-party auth providers.
